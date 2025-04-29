526329e7127161d957602d9850286d4121b4a174
Adrian <adrianruiz.campuslands@gmail.com>
2025-04-29 11:05:31 -0500

    Ajustes de la clase del 29

--- src/main/java/com/adrian/demojpa/domain/Person.java
+++ src/main/java/com/adrian/demojpa/domain/Person.java
@@ -8,12 +8,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name = "personas")
 @Getter
 @Setter
 public class Person {
